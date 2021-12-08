4cd8ccb573ff82fa231efaf0b482b25a72db7993
abinash88d <abinash88d@gmail.com>
2021-12-08 11:54:35 +0530

    Enhancements

--- src/main/java/com/crewmeister/cmcodingchallenge/commons/cache/CacheProperties.java
+++ src/main/java/com/crewmeister/cmcodingchallenge/commons/cache/CacheProperties.java
@@ -21,7 +21,7 @@ public class CacheProperties {
 	private String redisHost;
 
 	public CacheProperties(@Value("${PORT}") int redisPort,
-			@Value("${REDIS_URL}") String redisHost) {
+			@Value("${spring.redis.host}") String redisHost) {
 		this.redisPort = redisPort;
 		this.redisHost = redisHost;
 	}
